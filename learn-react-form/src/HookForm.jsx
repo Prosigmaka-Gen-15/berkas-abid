053083b173106f8cb9781f53ab8cc1d20c073c55
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-08-05 10:20:08 +0700

    fix: clean up HookForm code

--- learn-react-form/src/HookForm.jsx
+++ learn-react-form/src/HookForm.jsx
@@ -2,9 +2,7 @@ import { useState } from "react"
 import { useForm } from "react-hook-form"
 
 export default function HookForm() {
-	const { register, handleSubmit, formState: { errors } } = useForm()
-
-	console.log(errors)
+	const { register, handleSubmit } = useForm()
 
 	const onSubmit = (data) => {
 		console.log(data)
@@ -14,8 +12,7 @@ export default function HookForm() {
 		<form onSubmit={handleSubmit(onSubmit)}>
 			<section>
 				Nama:
-				<input type="text" {...register('nama', { required: true, minLength: 5 })} />
-				{errors.nama ? 'namanya error' : 'namanya sudah betul'}
+				<input type="text" {...register('nama')} />
 			</section>
 			<br />
 			<section>
